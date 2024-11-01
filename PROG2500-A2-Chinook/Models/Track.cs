3c59a9a9ccf4d26d03f90d91010c484b3d07c0ff
Chris Charles <w0481147@nscc.ca>
2024-11-01 11:23:49 -0300

    Created a partial class of tracks to handle string formatting

---
+++ PROG2500-A2-Chinook/Models/Track.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2500_A2_Chinook.Models
+{
+    public partial class Track
+    {
+        public string FormattedComposer 
+        {
+            get { return "Composer: " + (this.Composer ?? "None found."); }
+        }
+
+        public string FormattedLength
+        {
+            get { return "Length: " + this.Milliseconds.ToString("N0") + " ms"; }
+        }
+        public string FormattedBytes
+        {
+            get { return "Size: " + this.Bytes?.ToString("N0") + " bytes"; }
+        }
+        public string FormattedPrice
+        {
+            get { return "Price: $" + this.UnitPrice.ToString("N"); }
+        }
+    }
+}

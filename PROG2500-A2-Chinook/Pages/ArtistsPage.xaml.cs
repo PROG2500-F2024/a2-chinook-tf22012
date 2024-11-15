ac84e624b91dbcce3323609dd7d84602ad1a4757
Chris Charles <w0481147@nscc.ca>
2024-11-14 23:43:19 -0400

    Updated artists page to include search functionality

--- PROG2500-A2-Chinook/Pages/ArtistsPage.xaml.cs
+++ PROG2500-A2-Chinook/Pages/ArtistsPage.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Windows.UI;
 
 namespace PROG2500_A2_Chinook.Pages
 {
@@ -22,7 +23,7 @@ namespace PROG2500_A2_Chinook.Pages
     /// </summary>
     public partial class ArtistsPage : Page
     {
-        ChinookContext context = new ChinookContext();
+        ChinookContext _context = new ChinookContext();
         CollectionViewSource artistsViewSource = new CollectionViewSource();
 
         public ArtistsPage()
@@ -33,10 +34,21 @@ namespace PROG2500_A2_Chinook.Pages
             artistsViewSource = (CollectionViewSource)FindResource(nameof(artistsViewSource));
 
             //Use the dbContext to tell EF to load the data we want to use on this page.
-            context.Artists.Load();
+            _context.Artists.Load();
 
             //Set the viewsource data source to use the artists data collection (dbset)
-            artistsViewSource.Source = context.Artists.Local.ToObservableCollection();
+            artistsViewSource.Source = _context.Artists.Local.ToObservableCollection();
+        }
+
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var artistsQuery = 
+               from artist in _context.Artists
+               where artist.Name.Contains(artistSearchBox.Text)
+               orderby artist.Name
+               select artist;
+
+            artistsViewSource.Source = artistsQuery.ToList();
         }
     }
 }

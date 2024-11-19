91f7afc46414c10a7eefabb6c8cbd37cfb4d8038
Chris Charles <w0481147@nscc.ca>
2024-11-18 22:45:35 -0400

    Completed MusicCatalogPage functionality

--- PROG2500-A2-Chinook/Pages/MusicCatalogPage.xaml.cs
+++ PROG2500-A2-Chinook/Pages/MusicCatalogPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using PROG2500_A2_Chinook.Data;
+using PROG2500_A2_Chinook.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,14 +23,43 @@ namespace PROG2500_A2_Chinook.Pages
     /// </summary>
     public partial class MusicCatalogPage : Page
     {
+        private readonly ChinookContext _context = new ChinookContext();
+        private CollectionViewSource catalogViewSource;
         public MusicCatalogPage()
         {
             InitializeComponent();
+            catalogViewSource = (CollectionViewSource)FindResource(nameof(catalogViewSource));
+
+            _context.Artists.Load();
+            _context.Albums.Load();
+            _context.Tracks.Load();
+
+            catalogViewSource.Source = _context.Artists.Local.ToObservableCollection();
+
+            LoadCatalog();
         }
 
         private void SearchBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            LoadCatalog(catalogSearchBox.Text);
         }
+
+        private void LoadCatalog(string searchText = "")
+        {
+            // Define a grouping query to get grouped category data
+            var query =
+                from artist in _context.Artists
+                where string.IsNullOrEmpty(searchText) || artist.Name.Contains(searchText)
+                group artist by artist.Name.ToUpper().Substring(0, 1) into artGroup
+                select new
+                {
+                    HeaderText = $"{artGroup.Key} ({artGroup.Count()}):",
+                    artists = artGroup.ToList<Artist>(),
+                };
+
+            // Execute the query against the database and assign it as the data source for the list view
+            catalogListView.ItemsSource = query.ToList();
+        }
+
     }
 }
